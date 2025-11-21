23368834bb8e50e6568df21b60c87d3eae934bc7
stella <stellamplau@users.noreply.github.com>
2025-11-21 16:10:24 -0500

    Add update and contexts

--- lang/Syntax.v
+++ lang/Syntax.v
@@ -11,36 +11,25 @@ Set Asymmetric Patterns.
 
 Open Scope Z_scope.
 
-Inductive type :=
-| Bool
-| Bits (sz: N)
-| Pair (t1 t2: type)
-| Array (t: type) (sz: nat).
-
-Fixpoint type_denote tau : Type :=
-  match tau with
-  | Bool => bool
-  | Bits sz => bv sz
-  | Pair t1 t2 => (type_denote t1) * (type_denote t2)
-  | Array ty n => vec (type_denote ty) n 
-  end.
-
-(* Global Instance type_eq_dec : EqDecision type. *)
-(* Proof. solve_decision. Defined. *)
-
-(* Global Instance EqDecision_type_denote {tau: type} : EqDecision (type_denote tau). *)
-(* Proof. *)
-(*   unfold EqDecision. revert tau. *)
-(*   fix eq_dec_td 1. *)
-(*   destruct tau; cbn; try solve_decision. *)
-(*   eapply @vec_dec. *)
-(*   solve_decision. *)
-(* Defined. *)
-
+Module type.
+  Inductive type :=
+  | Bool
+  | Bits (sz: N)
+  | Pair (t1 t2: type)
+  | Array (t: type) (sz: nat).
+  
+  Fixpoint type_denote tau : Type :=
+    match tau with
+    | Bool => bool
+    | Bits sz => bv sz
+    | Pair t1 t2 => (type_denote t1) * (type_denote t2)
+    | Array ty n => vec (type_denote ty) n 
+    end.
+End type.
+Import type.
 Coercion type_denote: type >-> Sortclass.
 
 (* TODO: structs & array ops *)
-
 Module unop.
   Inductive unop : type -> type -> Type :=
   | Not {n} : unop (Bits n) (Bits n)
@@ -97,6 +86,22 @@ Module binop.
     end. 
 End binop.
 
+Module typeWithHole.
+  Inductive typeWithHole :=
+  | HOLE
+  | PairL (t1 : typeWithHole) (t2: type)
+  | PairR (t1 : type) (t2: typeWithHole)
+  | Array (sz: nat) (idx: fin sz) (t: typeWithHole).
+
+  Fixpoint plug (C : typeWithHole) (t : type) : type :=
+    match C with
+    | HOLE => t
+    | PairL t1 t2 => type.Pair (plug t1 t) t2
+    | PairR t1 t2 => type.Pair t1 (plug t2 t)
+    | Array sz _ t' => type.Array (plug t' t) sz
+    end.
+End typeWithHole.
+
 Module expr.
   Section WithSubstitutionType.
     Context {var: type -> Type}.
@@ -110,12 +115,23 @@ Module expr.
     | Const {tc: type} (c: type_denote tc) : expr tc
     | Unop {t1 tR: type} (op: unop t1 tR) (e1: expr t1) : expr tR
     | Binop {t1 t2 tR: type} (op: binop t1 t2 tR) (e1: expr t1) (e2: expr t2) : expr tR
-    | ITE {k: type} (cond: expr Bool) (tbranch fbranch: expr k) : expr k.
-    (* | LetIn {tx: type} (ex: expr tx) {tC: type} (eC: var tx -> expr tC) : expr tC. *)
+    | ITE {k: type} (cond: expr Bool) (tbranch fbranch: expr k) : expr k
+    | LetIn (name_hint: string) {tx: type} (ex: expr tx) {tC: type} (eC: var tx -> expr tC) : expr tC
+    | LetUpdate {C} {te} (e: expr (typeWithHole.plug C te)) (v: expr te): expr (typeWithHole.plug C te).
 
   End WithSubstitutionType.
 
   Arguments expr : clear implicits.
+  Fixpoint fieldUpdate {C: typeWithHole.typeWithHole} {t: type} 
+    (r : type_denote (typeWithHole.plug C t)) (v : type_denote t) 
+    : (type_denote (typeWithHole.plug C t)) :=
+    match C return type_denote (typeWithHole.plug C t) -> (type_denote (typeWithHole.plug C t)) with
+    | typeWithHole.HOLE => fun r => v
+    | typeWithHole.PairL t1 t2 => fun r => (fieldUpdate r.1 v, r.2)
+    | typeWithHole.PairR t1 t2 => fun r => (r.1, fieldUpdate r.2 v)
+    | typeWithHole.Array sz idx t => fun r => 
+         Vector.replace r idx (fieldUpdate (r !!! idx) v)
+    end r.
 
   Fixpoint interp {t} (e: expr type_denote t) : type_denote t :=
     match e in expr _ t return type_denote t with
@@ -125,8 +141,12 @@ Module expr.
     | Binop _ _ _ op e1 e2 => binop.interp op (interp e1) (interp e2)
     | ITE _ cond tbranch fbranch => 
         if interp cond then interp tbranch else interp fbranch
-    (* | ELetExp _ ex _ eC => let x := interp ex in interp (eC x) *)
-    end.  
+    | LetIn _ _ ex _ eC => let x := interp ex in interp (eC x)
+    | LetUpdate _ _ e v => 
+        let interp_e := interp e in 
+        let interp_v := interp v in 
+        fieldUpdate interp_e interp_v 
+    end.
 
 End expr.
 
@@ -138,12 +158,22 @@ Module action.
 
     Import expr.
 
-    Inductive action : type -> Type := 
+    Inductive action : type -> Type :=
+    (* | ReadState   *)
+    (* | Bind (name_hint: string) {tx: type} (ex: expr var tx) {tC: type} (eC: var tx -> action tC) : action tC *)
+    | LetAction (name_hint: string) {tx: type} (x: action tx) {tC: type} (eC: var tx -> action tC) : action tC
+
+    (* | If  *)
+    (* | IfElse (s: string) (p: Expr Bool) k' (t f: Action k') (cont: ty k' -> Action k) *)
+
     | Return {t: type} (e: expr var t) : action t
     .
   End WithSubstitutionType.
 
   Arguments action : clear implicits.
+
+  Definition env_t : Type.
+  Admitted.
 
   (* Fixpoint interp {t} (e: action type_denote t) : type_denote t := *)
   (*   match e with *)
@@ -216,3 +246,15 @@ End circuitSyntax.
 Module compiler.
   Import circuitSyntax.
 End compiler.
+
+(* Global Instance type_eq_dec : EqDecision type. *)
+(* Proof. solve_decision. Defined. *)
+
+(* Global Instance EqDecision_type_denote {tau: type} : EqDecision (type_denote tau). *)
+(* Proof. *)
+(*   unfold EqDecision. revert tau. *)
+(*   fix eq_dec_td 1. *)
+(*   destruct tau; cbn; try solve_decision. *)
+(*   eapply @vec_dec. *)
+(*   solve_decision. *)
+(* Defined. *)
